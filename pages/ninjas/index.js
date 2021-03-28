fdd7ab61daf121c61c36841d29044081ecd22e62
Dylan Morison <dylan@g.morison.org>
2021-03-27 21:01:31 -0700

    lesson 11 commit

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,8 +1,36 @@
-const Ninjas = () => {
+import Link from "next/link";
+import Head from "next/head";
+import styles from "../../styles/Ninja.module.css";
+
+export const getStaticProps = async () => {
+	const res = await fetch("https://jsonplaceholder.typicode.com/users");
+	const data = await res.json();
+
+	return {
+		props: {
+			ninjas: data
+		}
+	};
+};
+
+const Ninjas = (props) => {
 	return (
-		<div>
-			<h1>All Ninjas</h1>
-		</div>
+		<>
+			<Head>
+				<title>Ninja List | Home</title>
+				<meta name="keywords" keywords="ninja" />
+			</Head>
+			<div>
+				<h1>All Ninjas</h1>
+				{props.ninjas.map((ninja) => (
+					<Link href={`/ninjas/` + ninja.id} key={ninja.id}>
+						<a className={styles.single}>
+							<h3>{ninja.name}</h3>
+						</a>
+					</Link>
+				))}
+			</div>
+		</>
 	);
 };
 
