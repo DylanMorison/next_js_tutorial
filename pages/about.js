13076f13f062fb54c5256e11ff4a8ca6e7f7035a
Dylan Morison <dylan@g.morison.org>
2021-03-27 20:04:38 -0700

    init commit of next js tutorial

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+const About = () => {
+	return (
+		<div>
+			<h1>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam quas
+				voluptates nobis dignissimos, tempore optio officia hic, unde nisi nam nulla, quis
+				architecto quam quaerat sed omnis asperiores error.
+			</h1>
+			<h1>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam quas
+				voluptates nobis dignissimos, tempore optio officia hic, unde nisi nam nulla, quis
+				architecto quam quaerat sed omnis asperiores error.
+			</h1>
+		</div>
+	);
+};
+
+export default About;
